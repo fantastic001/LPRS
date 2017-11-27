4da53d70fa8dfb0dda70b461e1adb4cc8f95c36b
Stefan Nožinić <stefan@lugons.org>
2017-11-27 23:38:36 +0100

    Fixing constants

--- zadatak3/zadatak3.vhd
+++ zadatak3/zadatak3.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company:        FTN
--- Engineer:       Mario Perić
--- 
--- Create Date:    15:34:29 11/28/2016 
--- Design Name: 
--- Module Name:    zadatak3 - Behavioral 
--- Project Name:   zadatak3
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
@@ -40,38 +21,40 @@ entity zadatak3 is
 end zadatak3;
 
 architecture Behavioral of zadatak3 is
-	constant cSEC : STD_LOGIC_VECTOR (24 downto 0) := '1'&x"6E35FF";
-	signal sSEC : STD_LOGIC_VECTOR (7 downto 0);
-	signal sRUN : STD_LOGIC;
-	signal sRUN_old : STD_LOGIC;
-	signal sCOUNT : STD_LOGIC_VECTOR (24 downto 0);
+	constant cSEC : STD_LOGIC_VECTOR (24 downto 0) := "1011011100011011000000000"; -- broj klokova koji prodje posle jedne sekunde
+	-- konstanta inzad je 24 miliona 
+	
+	signal sSEC : STD_LOGIC_VECTOR (7 downto 0); -- registar koji čuva broj sekundi
+	signal sRUN : STD_LOGIC; -- stanje koje odredjuje da li je brojanje ukljuceno
+	signal sRUN_old : STD_LOGIC; -- cuva staro stanje RUN-a tj posle svakog takta pamti stanje za RUN
+	signal sCOUNT : STD_LOGIC_VECTOR (24 downto 0); -- broji taktove 
 
 begin
 		
 	process (sRUN, sRUN_old, inSTART, inCONTINUE, inSTOP, inRST) begin
-		if (inRST = '0') then
+		if (inRST = '0') then -- ako je reset pritisnut, prestajemo da brojimo
 			sRUN <= '0';
-		elsif (inSTART = '0') then
+		elsif (inSTART = '0') then -- ako je start pritisnut, pocinjemo da brojimo 
 			sRUN <= '1';
-		elsif (inSTOP = '0') then
+		elsif (inSTOP = '0') then -- ako je stop pritisnut, prestajemo da brojimo i ne diramo trenutno stanje sSEC i sCNT da bismo kasnije mogli da nastavimo 
 			sRUN <= '0';
-		elsif (inCONTINUE = '0') then
+		elsif (inCONTINUE = '0') then -- nastavljamo da brojimo 
 			sRUN <= '1';
 		else
-			sRUN <= sRUN_old;
+			sRUN <= sRUN_old; -- u svakom drugom slucaju uzimamo vec postojece (staro) stanje za RUN 
 		end if;
 	end process;
 	
 	process (iCLK, inRST) begin
-		if (inRST = '0') then
+		if (inRST = '0') then -- ako je reset pritisnut, vrati sve vrednosti na nulu 
 			sSEC <= (others => '0');
 			sCOUNT <= (others => '0');
 		elsif (rising_edge(iCLK)) then
-			if (sRUN = '1') then
+			if (sRUN = '1') then -- ako je stoperica pokreuta, povecavamo sCNT za 1 i sSEC ako je sCNT jednako broju taktova tokom jedne sekunde
 				if (sCOUNT = cSEC) then
 					sCOUNT <= (others => '0');
 					sSEC <= sSEC + "00000001";
-				elsif (inSTART = '0') then
+				elsif (inSTART = '0') then -- ako smo pritisnuli start, vracamo sve na 0
 					sSEC <= "00000000";
 					sCOUNT <= (others => '0');
 				else
--- zadatak3/zadatak3_tb.vhd
+++ zadatak3/zadatak3_tb.vhd
@@ -1,30 +1,3 @@
---------------------------------------------------------------------------------
--- Company:       FTN
--- Engineer:      Mario Perić
---
--- Create Date:   15:54:59 11/28/2016
--- Design Name:   
--- Module Name:   zadatak3 - Test Bench 
--- Project Name:  zadatak3
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: zadatak3
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
